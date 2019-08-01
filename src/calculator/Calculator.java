cf21bdc6b6e8d4a4b20ffa63d77f40616616ef2a
oui <oui@172.30.1.53>
2019-08-01 17:18:44 +0900

    initial commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+	public static void main(String[] args) {
+		System.out.println("덧셈:" + (3 + 4));
+		System.out.println("뺄셈:" + (4 - 3));
+		System.out.println("곱셈:" + (4 * 3));
+		System.out.println("나눗셈:" + (4 / 2));
+
+	}
+}
