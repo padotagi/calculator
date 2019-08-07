1c80321be15916702c3b07d46db6de91ece339d9
oui <oui@172.30.1.43>
2019-08-07 18:03:18 +0900

    계산기 기본 기능 구현 완

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -3,30 +3,45 @@ import java.util.*;
 
 public class Calculator {
 	public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	
-	System.out.println("첫번째 값: ");
-	int first = scanner.nextInt();
-	System.out.println(first);
-	
-	System.out.println("사칙연산 기호: ");
-	String symbol = scanner.next();
-	System.out.println(symbol);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("첫 번째 입력 값 : ");
+		int first = scanner.nextInt();		
+		System.out.println(first);
+		
+		int result = first;		
+		while (true) {
+			System.out.println("사칙연산 기호 : ");
+			String symbol = scanner.next();
+			System.out.println(symbol);
+				
+			if ("quit".equals(symbol)) {
+				System.out.println("최종 결과 값: " +result);
+				break;
+			}	
+			System.out.println("다음 입력  : ");
+			int second = scanner.nextInt();
+			System.out.println(second);
+			
+			if (symbol.equals("+")) {
+				result = result + second;
+				System.out.println("덧셈 : " + result);
+			} else if ("-".equals(symbol)) {
+				result = result - second;
+				System.out.println("뺄셈 : " + result);		
+			} else if ("*".equals(symbol)) {
+				result = result * second;
+				System.out.println("곱셈 : " + result);
+			} else if ("/".equals(symbol)) {	
+				result = result / second;
+				System.out.println("나눗셈 : " + result);
+			} else {
+			System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");	
 
-	System.out.println("두번째 값: ");
-	int second = scanner.nextInt();
-	System.out.println(second);
-	
-	if (symbol.equals("+")) {
-		System.out.println("덧셈: " + (first + second));
-	} else if ("-".equals(symbol)) {
-		System.out.println("뺄셈: " + (first - second));
-	} else if ("*".equals(symbol)) {
-		System.out.println("곱셈: " + (first * second));
-	} else if ("/".equals(symbol)) {	
-		System.out.println("나눗셈: " + (first / second));
-	} else {
-	System.out.println("사칙연산에 해당하지 않는 값을 입력했습니다.");	
 		}
+		
+		
 	}
+   }
 }
+  
